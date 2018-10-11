0bb98c67debd700f3437a77c9f187e832977a4d5
Justin Tan <justin@driver.xyz>
2018-10-11 23:46:12 +0800

    Organize dir

--- compile.sh
+++ compile.sh
@@ -2,8 +2,8 @@
 
 DIR=$(dirname "$0")
 
-protoc  -I ${DIR}/base/  ${DIR}/base/*.proto --go_out=plugins=grpc:${DIR}/
-rm -r ${DIR}/base/*.go
-mv ${DIR}/github.com/gopub/protobuf/base/*.go ${DIR}/base/
+protoc  -I ${DIR}/  ${DIR}/base.proto --go_out=plugins=grpc:${DIR}/
+rm -r ${DIR}/base
+mv ${DIR}/github.com/gopub/protobuf/base ${DIR}/
 rm -r ${DIR}/github.com
 
