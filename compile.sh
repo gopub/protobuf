418b84e3943a3c0920901b59ad85510d9cd566a7
JustinTan <mrnatande@gmail.com>
2018-11-29 10:46:53 +0800

    Remove pb

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@
 DIR=$(dirname "$0")
 
 protoc  -I ${DIR}/  ${DIR}/base.proto --go_out=plugins=grpc:${DIR}/
-rm -r ${DIR}/base
+rm -r ${DIR}/base/*.pb.go
 mv ${DIR}/github.com/gopub/protobuf/base ${DIR}/
 rm -r ${DIR}/github.com
 
